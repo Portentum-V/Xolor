b7a0e312117cf8a33d3586a6bc951ab941faf516
portentum.v <portentum.v@gmail.com>
2021-12-23 12:47:48 -0500

    init

---
+++ Xolor.py
@@ -0,0 +1,78 @@
+"""
+Xolor.py
+"""
+import os
+
+class Xolor:
+    """
+    Xolor:
+        A quick and dirty class for adding color to print statements
+        Should work on most linux distros and Windows 10+
+        
+    Old Windows Req?
+    if os.name == "nt": # Do this to get colors to work on Windows 10+
+        import subprocess
+        subprocess.call('', shell=True)
+
+    Notes:
+        Select graphic rendtions BOLD, FRAME, and CIRCLE did not work on Windows 10.
+        Additionally, SLOW and FAST blink were at the same speed
+    """
+    # Foreground Colors
+    GRAY    = '\033[90m'
+    RED     = '\033[91m'
+    GREEN   = '\033[92m'
+    YELLOW  = '\033[93m'
+    BLUE    = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN    = '\033[96m'
+    WHITE   = '\033[97m'
+    # Select Graphic Renditions
+    END     = '\033[0m' # Reset/Normal; default attributes
+    BOLD    = '\033[1m'
+    FAINT   = '\033[2m'
+    ITALIC  = '\033[3m'
+    UNDER   = '\033[4m'
+    SLOW    = '\033[5m' # Blink Slow
+    FAST    = '\033[6m' # Blink Fast
+    INVERT  = '\033[7m' # Swap foreground and background color
+    HIDE    = '\033[8m'
+    CROSS   = '\033[9m'
+    FRAME   = '\033[51m'
+    CIRCLE  = '\033[52m'
+    OVER    = '\033[53m'
+
+    # Disable specific SGR
+    NO_BOLD_OR_FAINT    = '\033[22m'
+    NO_ITALIC           = '\033[23m'
+    NO_UNDER            = '\033[24m'
+    NO_BLNK             = '\033[25m'
+    NO_INVERT           = '\033[27m'
+    NO_CROSS            = '\033[29m'
+    NO_FG               = '\033[39m' # Default foreground
+    NO_BG               = '\033[49m' # Default background
+    NO_FRAME_OR_CIRCLE  = '\033[54m'
+    NO_OVER             = '\033[55m'
+
+    # Formating
+    CRIT  =  MAGENTA + '[*] '
+    ERROR = RED      + '[!] '
+    WARN  = YELLOW   + '[?] '
+    INFO  = WHITE    + '[~] '
+    DEBUG = GRAY     + '[-] '
+    SUCC  = CYAN     + '[+] '
+    NICE  = GREEN    + '[$] '
+
+    # Normal FG ints; +10 for BG, +70 for Bright, +80 for BG Bright
+    black   = 30
+    red     = 31
+    green   = 32
+    yellow  = 33
+    blue    = 34
+    magenta = 35
+    cyan    = 36
+    white   = 37
+
+    # Lazy function
+    def color_str(self, c: str, s: str):
+        return(f"{c}{s}{self.END}")
