70312ac06402f662bf2bfd77300f7df77281f12d
portentum.v <portentum.v@gmail.com>
2021-12-24 20:09:31 -0500

    Added VERBOSE level, finished Xog class for decoration

--- Xog.py
+++ Xog.py
@@ -5,18 +5,45 @@ import logging
 import inspect 
 import sys
 
-from Xolor.Xolor import Xolor
+# lazy for testing import
+try:
+    from Xolor.Xolor import Xolor
+except ModuleNotFoundError:
+    from Xolor import Xolor
 
-class Xog_Format(logging.Formatter):
+# Source: https://newbedev.com/how-to-add-a-custom-loglevel-to-python-s-logging-facility
+def addLoggingLevel(level_str: str, level_int: int, method_str: str = None):
+    if not method_str:
+        method_str = level_str.lower()
+
+    if hasattr(logging, level_str):
+        raise AttributeError(f'{level_str} already defined in logging module')
+    if hasattr(logging, method_str):
+        raise AttributeError(f'{method_str} already defined in logging module')
+    if hasattr(logging.getLoggerClass(), method_str):
+        raise AttributeError(f'{method_str} already defined in logger class')
+        
+    def logForLevel(self, message, *args, **kwargs):
+        if self.isEnabledFor(level_int):
+            self._log(level_int, message, args, **kwargs)
+    def logToRoot(message, *args, **kwargs):
+        logging.log(level_int, message, *args, **kwargs)
+
+    logging.addLevelName(level_int, level_str)
+    setattr(logging, level_str, level_int)
+    setattr(logging.getLoggerClass(), method_str, logForLevel)
+    setattr(logging, method_str, logToRoot)
+
+class XogFormat(logging.Formatter):
     """
-    Xog_Format:
+    XogFormat:
         A log formatter class, used buy Xog to add Xolor spice based on log level.
     """
     xolor = Xolor()
 
     def __init__(self):
         super().__init__(fmt="%(asctime)s| %(funcName)s: %(lineno)d| %(message)s")
-        logger = logging.getLogger(log_name)
+        addLoggingLevel("VERBOSE", 5)
 
     def format(self, record):
         color = {
@@ -25,6 +52,7 @@ class Xog_Format(logging.Formatter):
             logging.WARN: self.xolor.WARN,
             logging.INFO: self.xolor.INFO,
             logging.DEBUG: self.xolor.DEBUG,
+            logging.VERBOSE: self.xolor.VERBOSE
         }.get(record.levelno, self.xolor.WEIRD)
         self._style._fmt = f"{color} %(asctime)s| %(funcName)s: %(lineno)d| %(message)s{self.xolor.END}"
         return super().format(record)
@@ -34,61 +62,57 @@ class Xog():
     Xog:
         A log decorator for doing log things!
     """
-    xolor = Xolor()
     log_level = 10
 
-    def __init__(self, log_name: str = __name__, log_level: int = 10, stream = sys.stdout):
+    def __init__(self, log_name: str = __name__, log_level: int = 5, stream = sys.stdout):
         self.log_name = log_name
         self.log_level = log_level
 
         self.logger = logging.getLogger(log_name)
-        self.logger.setLevel(log_level)
 
-        self.add_handler(stream)
-        
-    def get_logger():
-        return self.logger
+        self.changeLevel(log_level)
+        self.addHandler(stream)
 
-    def change_level(log_level: int = 10):
+    def changeLevel(self, log_level: int = 10):
         self.logger.setLevel(log_level)
-        for hdlr in logger.handlers:
+        for hdlr in self.logger.handlers:
             hdlr.setLevel(log_level)
         
-    def add_handler(log_level: int = 10, stream):
+    def addHandler(self, stream):
         ch = logging.StreamHandler(stream) # console handler, logging default sys.stderr, func default sys.stdout
-        ch.setLevel(log_level)
-        ch.setFormatter(Xog_Format())
+        ch.setLevel(self.log_level)
+        ch.setFormatter(XogFormat())
         self.logger.addHandler(ch)
         return ch
 
-    def remove_handler(hdlr):
+    def removeHandler(self, hdlr):
         self.logger.handlers.remove(hdlr)
 
-    def cleanup_logger():
-        try:
-            self.logger.handlers.clear()
-        except Exception as e:
-            print(f"{Xolor.ERROR}{type(e).__name__} occured while clearing logger:{Xolor.END} {e}")
-        return
+    def clearHandlers(self):
+        self.logger.handlers.clear()
 
-    def log(func = None):
-        def wrapper(*args, **kwargs):
+    def getLogger(self):
+        return self.logger
+
+    def log(self, func = None):
+        def log_wrapper(*args, **kwargs):
             args_repr = [repr(a) for a in args]
             kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
             params = ", ".join(args_repr + kwargs_repr)
-            self.logger.debug(f"{func.__name__} called with args {params}")
+            self.logger.verbose(f"{func.__name__} called with args {params}")
             try:
                 result = func(*args, **kwargs)
-                self.logger.debug(f"{func.__name__} returned with {result}")
+                self.logger.verbose(f"{func.__name__} returned with {result}")
                 return result
             except Exception as e:
                 self.logger.exception(f"{type(e).__name__}: {e}")
                 raise e
-        return wrapper
+        return log_wrapper
 
-def log_tester(level:int = 1):
-    xog = Xog(log_level = level)
-    logger = xog.get_logger()
+xog = Xog(log_level = 5)
+@xog.log
+def dec_log_tester():
+    logger = xog.getLogger()
     print("Logger Critical:")
     logger.critical("CRIT HIT!")
     print("Logger Error:")
@@ -101,4 +125,28 @@ def log_tester(level:int = 1):
     logger.info("Mad information status!")
     print("Logger Debug:")
     logger.debug("Very specific detail! Nice nice nice")
+    print("Logger Verbose:")
+    logger.verbose("OH GOD TO MUCH")
     return
+
+def raw_log_tester(level:int = 1):
+    logger = xog.getLogger()
+    print("Logger Critical:")
+    logger.critical("CRIT HIT!")
+    print("Logger Error:")
+    logger.error("ERROR ERROR ERROR")
+    print("Logger Exception:")
+    logger.exception("An exception occured")
+    print("Logger Warning:")
+    logger.warning("Warning message here")
+    print("Logger Info:")
+    logger.info("Mad information status!")
+    print("Logger Debug:")
+    logger.debug("Very specific detail! Nice nice nice")
+    print("Logger Verbose:")
+    logger.verbose("OH GOD TO MUCH")
+    return
+
+def level_tester():
+    for level in range(0,50):
+        print(logging.getLevelName(level))
--- Xolor.py
+++ Xolor.py
@@ -17,24 +17,42 @@ class Xolor:
         Select graphic rendtions BOLD, FRAME, and CIRCLE did not work on Windows 10.
         Additionally, SLOW and FAST blink were at the same speed
     """
+    # Faint Foreground Colors
+    BLACK       = '\033[30m'
+    RUBY        = '\033[31m'
+    EMERALD     = '\033[32m'
+    ORANGE      = '\033[33m'
+    SAPPHIRE    = '\033[34m'
+    PURPLE      = '\033[35m'
+    AQUA        = '\033[36m'
+    SILVER      = '\033[37m'
+    # Fain Background Colors
+    BG_BLACK    = '\033[40m'
+    BG_RUBY     = '\033[41m'
+    BG_EMERALD  = '\033[42m'
+    BG_ORANGE   = '\033[43m'
+    BG_SAPPHIRE = '\033[44m'
+    BG_PURPLE   = '\033[45m'
+    BG_AQUA     = '\033[46m'
+    BG_SILVER   = '\033[47m'
     # Bright Foreground Colors
-    GRAY    = '\033[90m'
-    RED     = '\033[91m'
-    GREEN   = '\033[92m'
-    YELLOW  = '\033[93m'
-    BLUE    = '\033[94m'
-    MAGENTA = '\033[95m'
-    CYAN    = '\033[96m'
-    WHITE   = '\033[97m'
+    GRAY        = '\033[90m'
+    RED         = '\033[91m'
+    GREEN       = '\033[92m'
+    YELLOW      = '\033[93m'
+    BLUE        = '\033[94m'
+    MAGENTA     = '\033[95m'
+    CYAN        = '\033[96m'
+    WHITE       = '\033[97m'
     # Bright Background Colors
-    BG_GRAY    = '\033[100m'
-    BG_RED     = '\033[101m'
-    BG_GREEN   = '\033[102m'
-    BG_YELLOW  = '\033[103m'
-    BG_BLUE    = '\033[104m'
-    BG_MAGENTA = '\033[105m'
-    BG_CYAN    = '\033[106m'
-    BG_WHITE   = '\033[107m'
+    BG_GRAY     = '\033[100m'
+    BG_RED      = '\033[101m'
+    BG_GREEN    = '\033[102m'
+    BG_YELLOW   = '\033[103m'
+    BG_BLUE     = '\033[104m'
+    BG_MAGENTA  = '\033[105m'
+    BG_CYAN     = '\033[106m'
+    BG_WHITE    = '\033[107m'
 
     # Select Graphic Renditions
     END     = '\033[0m' # Reset/Normal; default attributes
@@ -74,15 +92,50 @@ class Xolor:
     white   = 37
 
     # Formating
-    CRIT  = MAGENTA + SLOW + '[*]'
-    ERROR = RED            + '[!]'
-    WARN  = YELLOW         + '[?]'
-    INFO  = WHITE + FAINT  + '[~]'
-    DEBUG = GRAY           + '[-]'
-    SUCC  = CYAN           + '[+]'
-    NICE  = GREEN          + '[$]'
-    WEIRD = BLUE           + '[ ]'
+    CRIT    = MAGENTA + '[*]'
+    ERROR   = RED     + '[!]'
+    WARN    = YELLOW  + '[?]'
+    INFO    = SILVER  + '[~]'
+    DEBUG   = GRAY    + '[-]'
+    VERBOSE = ORANGE  + '[v]'
+    SUCC    = CYAN    + '[+]'
+    NICE    = GREEN   + '[$]'
+    WEIRD   = BLUE    + '[ ]'
 
     # Lazy function
     def color_str(self, c: str, s: str):
         return(f"{c}{s}{self.END}")
+
+def color_test():
+    xolor = Xolor()
+    print(xolor.color_str(xolor.RUBY, "Ruby"))
+    print(xolor.color_str(xolor.EMERALD, "Emerald"))
+    print(xolor.color_str(xolor.ORANGE, "Orange"))
+    print(xolor.color_str(xolor.SAPPHIRE, "Sapphire"))
+    print(xolor.color_str(xolor.PURPLE, "Purple"))
+    print(xolor.color_str(xolor.AQUA, "Aqua"))
+    print(xolor.color_str(xolor.SILVER, "Silver"))
+    print(xolor.color_str(xolor.BG_BLACK, "Silver on Black"))
+    print(xolor.color_str(xolor.BG_RUBY, "BG Ruby"))
+    print(xolor.color_str(xolor.BG_EMERALD, "BG Emerald")) 
+    print(xolor.color_str(xolor.BG_ORANGE, "BG Orange"))
+    print(xolor.color_str(xolor.BG_SAPPHIRE, "BG Sapphire"))
+    print(xolor.color_str(xolor.BG_PURPLE, "BG Purple"))
+    print(xolor.color_str(xolor.BG_AQUA, "BG Aqua"))
+    print(xolor.color_str(xolor.BG_SILVER, f"{xolor.BLACK}FG Black on BG Silver"))
+    print(xolor.color_str(xolor.GRAY, "Gray"))
+    print(xolor.color_str(xolor.RED, "Red"))
+    print(xolor.color_str(xolor.GREEN, "Green"))  
+    print(xolor.color_str(xolor.YELLOW, "Yellow"))
+    print(xolor.color_str(xolor.BLUE, "Blue"))
+    print(xolor.color_str(xolor.MAGENTA, "Magenta"))
+    print(xolor.color_str(xolor.CYAN, "Cyan"))
+    print(xolor.color_str(xolor.WHITE, "White"))
+    print(xolor.color_str(xolor.BG_GRAY, "BG Gray"))
+    print(xolor.color_str(xolor.BG_RED, "BG Red"))
+    print(xolor.color_str(xolor.BG_GREEN, "BG Green"))
+    print(xolor.color_str(xolor.BG_YELLOW, "BG Yellow"))
+    print(xolor.color_str(xolor.BG_BLUE, "BG Blue"))
+    print(xolor.color_str(xolor.BG_MAGENTA, "BG Magenta"))
+    print(xolor.color_str(xolor.BG_CYAN, "BG Cyan"))
+    print(xolor.color_str(xolor.BG_WHITE, "BG White"))
